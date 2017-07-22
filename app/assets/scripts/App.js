6d12fd58a1e1b03cf4fe33b0194ab22e89fbc231
agatonsax <misaminic@gmail.com>
2017-07-22 15:42:31 +0200

    header finished

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,3 @@
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes $0 in Texas.");
-	}
-}
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "white");
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
